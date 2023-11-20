fc992f9272a0d2c06c7d4be7bf5ac4d8ec50c61b
Frenky233 <supertiger46590@gmail.com>
2023-11-20 19:13:39 +0500

    03 module all tasks

--- 03-objects-arrays-intro-to-testing/3-trim-symbols/index.js
+++ 03-objects-arrays-intro-to-testing/3-trim-symbols/index.js
@@ -5,5 +5,35 @@
  * @returns {string} - the new string without extra symbols according passed size
  */
 export function trimSymbols(string, size) {
+    if(size == 0){
+        return '';
+    }
+    if(!size){
+        return string;
+    }
+    if(!string){
+        return '';
+    }
+    
+    let splittedString = string.split('');
+    let endString = '';
+    let prevSymbol = 0;
+    let curSize = 1;
 
+    for(let symbol of splittedString){
+        if(symbol == prevSymbol){
+            curSize++;
+        }
+        else{
+            curSize = 1;
+        }
+
+        if(curSize <= size){
+            endString += symbol;
+        }
+
+        prevSymbol = symbol;
+    }
+
+    return endString;
 }
