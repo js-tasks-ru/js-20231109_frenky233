409206676634282013c8f9156cec00e0d0e42a78
Frenky233 <supertiger46590@gmail.com>
2023-12-11 12:01:29 +0500

    08 module all tasks

--- 08-forms-fetch-api-part-2/1-product-form-v1/index.js
+++ 08-forms-fetch-api-part-2/1-product-form-v1/index.js
@@ -5,11 +5,341 @@ const IMGUR_CLIENT_ID = '28aaa2e823b03b1';
 const BACKEND_URL = 'https://course-js.javascript.ru';
 
 export default class ProductForm {
+  categories;
+  element;
+  subElements;
+  
   constructor (productId) {
     this.productId = productId;
+    
+    this.formElements = {
+      title: null,
+      description: null,
+      subcategory: null,
+      price: null,
+      discount: null,
+      quantity: null,
+      status: null,
+    };
   }
 
-  async render () {
+  createFormTemplate(){
+    return `
+    <div class="product-form">
+      <form data-element="productForm" class="form-grid">
+        <div class="form-group form-group__half_left">
+          <fieldset>
+            <label class="form-label">Название товара</label>
+            <input required="" type="text" name="title" id='title' class="form-control" placeholder="Название товара">
+          </fieldset>
+        </div>
+        <div class="form-group form-group__wide">
+          <label class="form-label">Описание</label>
+          <textarea required="" class="form-control" name="description" id='description' data-element="productDescription" placeholder="Описание товара"></textarea>
+        </div>
+        <div class="form-group form-group__wide" data-element="sortable-list-container">
+          <label class="form-label">Фото</label>
+          <div data-element="imageListContainer">
+          </div>
+          <button type="button" name="uploadImage" id='uploadImage' data-element="uploadImage" class="button-primary-outline">
+            <span>Загрузить</span>
+          </button>
+        </div>
+        <div class="form-group form-group__half_left">
+          <label class="form-label">Категория</label>
+          ${this.createCategoriesElement()}
+        </div>
+        <div class="form-group form-group__half_left form-group__two-col">
+          <fieldset>
+            <label class="form-label">Цена ($)</label>
+            <input required="" type="number" name="price" id='price' class="form-control" placeholder="100">
+          </fieldset>
+          <fieldset>
+            <label class="form-label">Скидка ($)</label>
+            <input required="" type="number" name="discount" id='discount' class="form-control" placeholder="0">
+          </fieldset>
+        </div>
+        <div class="form-group form-group__part-half">
+          <label class="form-label">Количество</label>
+          <input required="" type="number" class="form-control" name="quantity" id='quantity' placeholder="1">
+        </div>
+        <div class="form-group form-group__part-half">
+          <label class="form-label">Статус</label>
+          <select class="form-control" name="status" id='status'>
+            <option value="1">Активен</option>
+            <option value="0">Неактивен</option>
+          </select>
+        </div>
+        <div class="form-buttons">
+          <button type="submit" name="save" class="button-primary-outline">
+            Сохранить товар
+          </button>
+        </div>
+      </form>
+    </div>
+    `;
+  }
 
+  createFormElement(){
+    const element = document.createElement('div');
+    element.innerHTML = this.createFormTemplate();
+
+    return element.firstElementChild;
+  }
+
+  createCategoryItemElement({id, title}, categoryTitle){
+    const element = new Option(categoryTitle + ' > ' + title, id);
+
+    return element.outerHTML;
+  }
+
+  createCategoriesElement(){
+    const element = document.createElement('select');
+    element.classList.add('form-control');
+    element.setAttribute('name', 'subcategory');
+    element.id = "subcategory";
+    
+    this.categories.forEach(({subcategories, title}) => 
+      subcategories.forEach((subCategory) => 
+        element.insertAdjacentHTML('beforeend', this.createCategoryItemElement(subCategory, title))));
+
+    return element.outerHTML;
+  }
+
+  createImageItemTemplate(source, url){
+    return `
+    <ul class="sortable-list">
+      <li class="products-edit__imagelist-item sortable-list__item" style="">
+        <input type="hidden" name="url" value="${url}">
+        <input type="hidden" name="source" value="${source}">
+        <span>
+          <img src="icon-grab.svg" data-grab-handle="" alt="grab">
+          <img class="sortable-table__cell-img" alt="Image" src="${url}">
+          <span>${source}</span>
+        </span>
+        <button type="button">
+          <img src="icon-trash.svg" data-delete-handle="" alt="delete">
+        </button>
+      </li>
+    </ul>
+    `;
+  }
+
+  addFormEventListener(){
+    this.element.addEventListener('submit', this.onFormSubmit)
+  }
+
+  addImageListEventListeners(){
+    this.subElements.imageListContainer.addEventListener('click', this.onDeleteImageClick);
+    this.subElements.uploadImage.addEventListener('click', this.onUploadImageClick);
+  }
+
+  removeImageListEventListeners(){
+    this.subElements.imageListContainer.removeEventListener('click', this.onDeleteImageClick);
+    this.subElements.uploadImage.removeEventListener('click', this.onUploadImageClick);
+  }
+
+  removeFormEventListener(){
+    this.element.removeEventListener('submit', this.onFormSubmit)
+  }
+
+  async render(){
+    this.categories = await this.getCategories();
+    this.element = this.createFormElement();
+    this.subElements = this.getSubElements();
+    this.getFormElements();
+
+    if(this.productId){
+      this.productData = await this.getData(this.productId);
+      this.editProductCard(this.productData[0]);
+    }
+    else{
+      this.createProductCard();
+    }
+
+    this.addFormEventListener();
+    this.addImageListEventListeners();
+
+    return this.element;
+  }
+
+  createProductCard(){
+    this.eventOnSubmit = new CustomEvent('product-saved', {
+      bubbles: true,
+    });
+  }
+
+  editProductCard(data){
+    this.setExistValues(data);
+
+    this.eventOnSubmit = new CustomEvent('product-updated', {
+      bubbles: true,
+    });
+  } 
+
+  setExistValues(data){
+    for(const [key, element] of Object.entries(this.formElements)){
+      element.value = data[key];
+    }
+    this.setImages(data.images);
+  }
+
+  addImageItemElement(source, url){
+    this.subElements.imageListContainer.insertAdjacentHTML('beforeend', this.createImageItemTemplate(source, url))
+  }
+
+  setImages(images){
+    images.forEach(({source, url}) => {
+      this.addImageItemElement(source, url)
+    });
+  }
+
+  getImages(){
+    const images = [];
+    
+    for(const element of this.subElements.imageListContainer.children){
+      const data = element.querySelectorAll('[name]');
+      images.push({source: escapeHtml(data[0].value), url: escapeHtml(data[1].value)});
+    }
+
+    return images;
+  }
+
+  checkValueType(key, value){
+    switch(typeof this.productData[key]){
+      case 'string':
+        return value;
+        break;
+      case 'number':
+        return +value;
+        break;
+    }
+  }
+
+  getFormData(){
+    const formData = {};
+    for(const [key, element] of Object.entries(this.formElements)){
+      formData[key] = this.checkValueType(key, element.value);
+    }
+    
+    formData.images = this.getImages();
+    formData.id = this.productId;
+
+    return formData;
+  }
+
+  onUploadImageClick = () =>{
+    const imageInput = document.createElement('input');
+    imageInput.type = 'file';
+    imageInput.accept = 'image/*';
+    imageInput.hidden = true;
+    
+    document.body.append(imageInput);
+
+    imageInput.onchange = async () =>{
+      const image = imageInput.files[0];
+
+      if(!image) return;
+
+      const formData = new FormData();
+      formData.append('image', image);
+
+      this.subElements.uploadImage.classList.add("is-loading");
+      this.subElements.uploadImage.disabled = true;
+
+      try{
+        const response = await fetchJson('https://api.imgur.com/3/image', {
+          method: 'POST',
+          headers: {
+            authorization: `Client-ID ${IMGUR_CLIENT_ID}`
+          },
+          body: formData
+        });
+
+        this.addImageItemElement(escapeHtml(image.name), response.data.link);
+      } catch(err){
+        throw new err;
+      } finally {
+        this.subElements.uploadImage.classList.remove("is-loading");
+        this.subElements.uploadImage.disabled = false;
+        imageInput.remove();
+      }
+    }
+
+    imageInput.click();
+  }
+
+  onDeleteImageClick = (event) =>{
+    if('deleteHandle' in event.target.dataset){
+      event.target.closest('UL').remove();
+    }
+  }
+
+  onFormSubmit = (event) =>{
+    event.preventDefault();
+    this.save()
+  }
+
+  save = async () =>{
+    const url = new URL('/api/rest/products', BACKEND_URL);
+
+    const formData = this.getFormData();
+
+    await fetchJson(url, {
+      method: this.productId ? "PATCH" : "PUT",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(formData)
+    });
+
+    this.element.dispatchEvent(this.eventOnSubmit);
+  }
+
+  async getCategories(){
+    const url = new URL('/api/rest/categories', BACKEND_URL);
+    url.searchParams.set('_sort', 'weight');
+    url.searchParams.set('_refs', 'subcategory');
+
+    const categories = await fetchJson(url);
+
+    return categories;
+  }
+
+  async getData(id){
+    const url = new URL('/api/rest/products', BACKEND_URL);
+    url.searchParams.set('id', id);
+
+    const data = await fetchJson(url);
+    
+    return data;
+  }
+
+  getSubElements(){
+    const subElements = {};
+    const elements = this.element.querySelectorAll('[data-element]');
+
+    elements.forEach(element => 
+        subElements[element.dataset.element] = element);
+
+    return subElements;
+  }
+
+  getFormElements(){
+    for(const item in this.formElements){
+      const element = this.element.querySelector(`[name=${item}]`);
+      this.formElements[item] = element;
+    }
+  }
+
+  remove(){
+    this.element.remove();
+  }
+
+  destroy(){
+    this.removeFormEventListener();
+    this.removeImageListEventListeners();
+
+    this.remove();
   }
 }
--- 08-forms-fetch-api-part-2/2-range-picker/index.js
+++ 08-forms-fetch-api-part-2/2-range-picker/index.js
@@ -1,3 +1,343 @@
 export default class RangePicker {
+    element;
+    
+    constructor({from = new Date(), to = new Date()} = {}){
+        this.from = this.removeUTCOffset(from);
+        this.to = this.removeUTCOffset(to);
+        this.selectedButtonElements = [];
+        this.calendarElementsData = {};
+        
+        this.clickCounter = 0;
+        this.newDate = [];
 
+        this.isRendered = false;
+
+        this.render();
+    }
+
+    createSelectorTemplate(from, to){
+        if(from.getMonth() == to.getMonth()){
+            to = new Date(new Date(to).setMonth(to.getMonth() + 1))
+        }
+
+        return `
+                <div class="rangepicker__selector-arrow"></div>
+                <div class="rangepicker__selector-control-left"></div>
+                <div class="rangepicker__selector-control-right"></div>
+                ${this.createCalendarTemplate(from)}
+                ${this.createCalendarTemplate(to)}
+        `
+    }
+
+    createRangePickerTemplate(){
+        return `
+        <div class="rangepicker">
+            <div class="rangepicker__input" data-element="input">
+                <span data-element="from">${this.from.toLocaleDateString()}</span> -
+                <span data-element="to">${this.to.toLocaleDateString()}</span>
+            </div>
+            <div class="rangepicker__selector" data-element="selector"></div>
+        </div>
+        `;
+    }
+
+    createCalendarTemplate(date){
+        return `
+        <div class="rangepicker__calendar"'>
+            <div class="rangepicker__month-indicator">
+            <time datetime="${date.toLocaleString('en', { month: 'long' })}">${date.toLocaleString('ru', { month: 'long' })}</time>
+            </div>
+            <div class="rangepicker__day-of-week">
+            <div>Пн</div>
+            <div>Вт</div>
+            <div>Ср</div>
+            <div>Чт</div>
+            <div>Пт</div>
+            <div>Сб</div>
+            <div>Вс</div>
+            </div>
+            ${this.createDateGridTemplate(date)}
+        </div>
+        `
+    }
+
+    createDateGridTemplate(date){
+        let inner = '';
+
+        for(let day = 2; day <= this.daysInMonth(date); day++){
+            inner += this.createDateGridItemTempalte(new Date(new Date(date).setDate(day)));
+        }
+        
+        return `
+        <div class="rangepicker__date-grid">
+        <button type="button" class="rangepicker__cell" style="--start-from: ${this.getFirstDayOfMonth(date)}" data-value="${(new Date(new Date(date).setDate(1)).toJSON())}">1</button>
+            ${inner}
+        </div>
+        `
+    }
+
+    createDateGridItemTempalte(date){
+        return `<button type="button" class="rangepicker__cell" data-value="${date.toJSON()}">${date.getDate()}</button>`;
+    }
+
+    createRangePickerElement(){
+        const element = document.createElement('div');
+        element.innerHTML = this.createRangePickerTemplate();
+
+        return element.firstElementChild;
+    }
+
+    render(){
+        this.element = this.createRangePickerElement();
+        this.subElements = this.getSubElements();
+
+        this.addInputEventListener();
+    }
+
+    open(){
+        this.addSelectorElementEventListeners();
+        this.addDocumentEventListener();
+
+        this.element.classList.toggle('rangepicker_open');
+        
+        if(!this.isRendered){
+            this.subElements.selector.innerHTML = this.createSelectorTemplate(this.from, this.to);
+        
+            const calendarElements = this.getCalendarElements();
+            this.calendarElementsData = {
+                left: {
+                    element: calendarElements.left,
+                    date: new Date(new Date(this.from).setDate(1))
+                },
+                right: {
+                    element: calendarElements.right,
+                    date: new Date(new Date(this.to).setDate(1))
+                }
+            }
+            
+            this.setTimeInterval(this.from, this.to);
+            this.isRendered = true;
+        }
+    }
+
+    close(){
+        this.element.classList.remove('rangepicker_open');
+        this.removeSelectorElementEventListeners();
+        this.removeDocumentEventListener();
+    }
+
+    addSelectorElementEventListeners(){
+        this.subElements.selector.addEventListener('click', this.onControlElementClick);
+        this.subElements.selector.addEventListener('click', this.onDayElementClick);
+    }
+
+    addInputEventListener = () =>{
+        this.subElements.input.addEventListener('click', this.onInputElementClick);
+    }
+
+    addDocumentEventListener(){
+        document.addEventListener('click', this.onDocumentClick, {capture: true});
+    }
+
+    removeDocumentEventListener(){
+        document.removeEventListener('click', this.onDocumentClick, {capture: true});
+    }
+
+    removeInputEventListener(){
+        this.subElements.input.removeEventListener('click', this.onInputElementClick);
+    }
+
+    removeSelectorElementEventListeners(){
+        this.subElements.selector.removeEventListener('click', this.onControlElementClick);
+        this.subElements.selector.removeEventListener('click', this.onDayElementClick);
+    }
+
+    onDocumentClick = (event) =>{
+        const targetElement = event.target;
+        
+        if(!targetElement.closest('.rangepicker__selector') && !targetElement.closest('[data-element="input"]')){
+            this.close();
+        }
+    }
+
+    onInputElementClick = () =>{
+        if(!this.element.classList.contains('rangepicker_open')){
+            this.open();
+            this.addDocumentEventListener();
+        } else {
+            this.close();
+        }
+    }
+
+    onControlElementClick = (event) =>{
+        const targetElement = event.target;
+
+        if(targetElement.classList.contains('rangepicker__selector-control-left')){
+            this.doCalendarSwitch('left');
+        } 
+        else if(targetElement.classList.contains('rangepicker__selector-control-right')){
+            this.doCalendarSwitch('right');
+        }
+
+        this.setTimeInterval(this.from, this.to);
+    }
+
+    onDayElementClick = (event) =>{
+        const buttonElement = event.target.closest('.rangepicker__cell');
+        
+        if(!buttonElement) return;
+        
+        const newDate = new Date(buttonElement.dataset.value);
+        
+        switch(this.clickCounter){
+            case 0:
+                this.newDate.push(newDate);
+                this.clickCounter++;
+                break;
+            case 1:
+                this.newDate.push(newDate);
+                this.clickCounter = 0;
+                break;
+        }
+
+        if(this.selectedButtonElements){
+            this.selectedButtonElements.forEach(element => element.className = 'rangepicker__cell');
+        }
+
+        this.selectedButtonElement = [];
+        
+        if(this.newDate.length == 2){
+            this.from = new Date(Math.min(...this.newDate));
+            this.to = new Date(Math.max(...this.newDate));
+            this.newDate = [];
+
+            this.setTimeInterval(this.from, this.to);
+            this.dispatchEvent();
+        }        
+    }
+
+    doCalendarSwitch(dir){
+        const dirToDelete = dir == 'left' ? 'right' : 'left';
+        const placeToInsertHTML = dir == 'left' ? 'beforebegin' : 'afterend';
+        const newMonth = dir == 'left' ? -1 : 1;
+        
+        this.calendarElementsData[dirToDelete].element.remove();
+        this.calendarElementsData[dirToDelete].element = this.calendarElementsData[dir].element;
+        this.calendarElementsData[dirToDelete].date = new Date(this.calendarElementsData[dir].date);
+        
+        this.calendarElementsData[dir].date = new Date(new Date(this.calendarElementsData[dir].date.setMonth(this.calendarElementsData[dir].date.getMonth() + newMonth)));
+        this.calendarElementsData[dirToDelete].element.insertAdjacentHTML(placeToInsertHTML, this.createCalendarTemplate(this.calendarElementsData[dir].date));
+        
+        switch(dir){
+            case 'left':
+                this.calendarElementsData[dir].element = this.calendarElementsData[dirToDelete].element.previousSibling.previousSibling;
+                break;
+            case 'right':
+                this.calendarElementsData[dir].element = this.calendarElementsData[dirToDelete].element.nextSibling.nextSibling;
+                break;
+        }
+    }
+
+    getCalendarElements(){
+        const elementsArray = this.element.querySelectorAll('.rangepicker__calendar');
+        const elementsObj = {
+            left: elementsArray[0],
+            right: elementsArray[1]
+        }
+
+        return elementsObj;
+    }
+
+    getSubElements(){
+        const subElements = {};
+        const elements = this.element.querySelectorAll('[data-element]');
+    
+        elements.forEach(element => 
+            subElements[element.dataset.element] = element);
+    
+        return subElements;
+    }
+
+    validateDates(from, to){
+        const dates = [];
+        this.element.querySelectorAll('.rangepicker__cell').forEach(element => dates.push(new Date(element.dataset.value)));
+
+        from = new Date(Math.max(from, Math.min(...dates))).toJSON();
+        to = new Date(Math.min(to, Math.max(...dates))).toJSON();
+
+        const outOfBounce = dates.find(date => date.toJSON() == from) && dates.find(date => date.toJSON() == to);
+
+        return [from, to, outOfBounce];
+    }
+
+    setTimeInterval(from, to){
+        this.subElements.from.textContent = from.toLocaleDateString();
+        this.subElements.to.textContent = to.toLocaleDateString();
+        
+        let outOfBounce = false;
+        [from, to, outOfBounce] = this.validateDates(from, to);
+
+        if(!outOfBounce) return;
+        
+        let date = new Date(from);
+
+        while(date.toJSON() != to){
+            const buttonElementToPaint = this.element.querySelector(`[data-value="${date.toJSON()}"]`);
+            this.selectedButtonElements.push(buttonElementToPaint);
+
+            buttonElementToPaint.classList.add('rangepicker__selected-between');
+
+            date = new Date(new Date(date).setDate(date.getDate() + 1));
+        }
+
+        if(from == this.from.toJSON()){
+            const startElement = this.element.querySelector(`[data-value="${from}"]`);
+            startElement.className = 'rangepicker__cell rangepicker__selected-from';
+        }
+        
+        if(to == this.to.toJSON()){
+            const endElement = this.element.querySelector(`[data-value="${to}"]`);
+            endElement.className = 'rangepicker__cell rangepicker__selected-to';
+            this.selectedButtonElements.push(endElement);
+        }
+    }
+
+    dispatchEvent(){
+        this.element.dispatchEvent(
+          new CustomEvent("date-select", {
+            bubbles: true,
+            detail: {
+                from: this.from.toJSON(),
+                to: this.to.toJSON()
+            },
+          })
+        );
+    }
+
+    removeUTCOffset(date){
+        return new Date(date.setMinutes(date.getMinutes() - date.getTimezoneOffset()));
+    }
+
+    daysInMonth(date) {
+        return new Date(date.getYear(), date.getMonth() + 1, 0).getDate();
+    }
+
+    getFirstDayOfMonth(date){
+        let day = new Date(new Date(date).setDate(1)).getDay();
+        
+        day = day ? day : 7;
+
+        return day; 
+    }
+
+    remove(){
+        this.element.remove();
+        this.removeDocumentEventListener();
+        this.removeInputEventListener();
+        this.removeSelectorElementEventListeners();
+    }
+    
+    destroy(){
+        this.remove();
+    }
 }
